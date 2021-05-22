9d12399c1aeb52ae03db48b81dd3cb479ef5c748
Andrew Barber <andrew.ab@gmail.com>
2021-05-22 02:29:07 +0100

    script bug fix now working

--- trans-xy.py
+++ trans-xy.py
@@ -1,5 +1,5 @@
 import argparse
-from XY_433 import XY_4333_trans
+from XY_433 import XY_433_trans
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('pin', type=int, help='Raspberry Pi Broadcom GPIO pin number')
@@ -7,7 +7,7 @@ parser.add_argument('code', type=int, help='Command to transmit')
 parser.add_argument('-b', '--bits', type=int, default=24, help='Number of bits in command')
 parser.add_argument('-r', '--repeat', type=int, default=5, help='Number of repeated transmissions')
 args = parser.parse_args()
-trans = XY_433_trans(arg.pin)
+trans = XY_433_trans(args.pin)
 
 trans.XY_send(args.code, args.bits, args.repeat)
-print("transmission successfull")
+print("transmission complete")
